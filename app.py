5ddfe25ad638bda58eb66680bc56f58c5a6706f1
martin-arcpi <104268393+martin-arcpi@users.noreply.github.com>
2025-03-21 14:11:07 +0000

    Add credit score column.

--- app.py
+++ app.py
@@ -16,8 +16,12 @@ last_names = ['Ma', 'Das', 'Pham', 'Duong',
               'Pérez', 'Yadav', 'Sánchez', 'Joshi',
               'Kumar', 'He', 'Wilson', 'Yoon']
 
-with open('data.csv', 'w') as file:
+num_of_customers = 10
+
+credit_scores = [randint(400, 999) for score in range(0,10)]
+
+with open('credit_scores.csv', 'w') as file:
     writer = csv.writer(file)
-    writer.writerow(['first_name', 'last_name',])
-    for i in range(10):
-        writer.writerow([choice(first_names), choice(last_names)])
+    writer.writerow(['first_name', 'last_name', 'credit_score'])
+    for i in range(num_of_customers):
+        writer.writerow([choice(first_names), choice(last_names), choice(credit_scores)])
