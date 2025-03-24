d25c9e701cd79a8d4d275cfaa0423a48835e798e
martin-arcpi <104268393+martin-arcpi@users.noreply.github.com>
2025-03-24 14:01:22 +0000

    Add full list of countries.

--- app.py
+++ app.py
@@ -20,11 +20,13 @@ last_names = ['Ma', 'Das', 'Pham', 'Duong',
 
 shuffle(last_names)
 
-# TODO add age and dob column using timedeltas
+
 customer_count = 100
 
 credit_scores = [randint(400, 999) for score in range(0,10)]
 
+
+# TODO Fix value error that occurs to cause out of range for month
 birth_dates = []
 for i in range(customer_count):
     birth_dates.append(date(randint(1937, 2005), randint(1, 12), randint(1, 30)))
@@ -41,15 +43,40 @@ employment_status = ['Employed', 'Self-Employed', 'Unemployed', 'Student']
 # ]
 # shuffle(cities)
 
-
 countries = [
-    "China", "India", "United States", "Indonesia", "Pakistan",
-    "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico",
-    "Canada", "Australia", "Argentina", "South Korea", "Spain",
-    "Poland", "Saudi Arabia", "Netherlands", "Thailand", "Turkey",
-    "Iceland", "Luxembourg", "Malta", "Fiji", "Bhutan",
-    "Monaco", "Liechtenstein", "San Marino", "Andorra", "Seychelles"
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", 
+    "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", 
+    "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", 
+    "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", 
+    "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", 
+    "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", 
+    "Congo (Democratic Republic of the Congo)", "Costa Rica", "Croatia", "Cuba", 
+    "Cyprus", "Czechia (Czech Republic)", "Denmark", "Djibouti", "Dominica", "Dominican Republic", 
+    "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", 
+    "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", 
+    "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", 
+    "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", 
+    "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", 
+    "Korea (North)", "Korea (South)", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", 
+    "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", 
+    "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", 
+    "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", 
+    "Morocco", "Mozambique", "Myanmar (formerly Burma)", "Namibia", "Nauru", "Nepal", "Netherlands", 
+    "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", 
+    "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", 
+    "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", 
+    "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", 
+    "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", 
+    "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", 
+    "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", 
+    "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", 
+    "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", 
+    "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", 
+    "Yemen", "Zambia", "Zimbabwe"
 ]
+
+
+
 shuffle(countries)
 
 with open('credit_scores.csv', 'w') as file:
